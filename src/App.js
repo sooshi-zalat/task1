332e46077a2bd35ae0e0650069c0e5bdedfc3bc2
EsraaZalat <esraasherifzalat@gmail.com>
2021-12-24 14:59:50 +0200

    clean

--- src/App.js
+++ src/App.js
@@ -9,26 +9,10 @@ class App extends Component  {
   return (
     <div className="App">
       <Cascading/>
-      <Signin />
+      <Signin/>
     </div>
   );
   }
-  state = {
-    schools: [], 
-    classes:[],
-    user:[],
-    key:[]
-};
-
-componentDidMount() {
   
-  fetch('https://react-task-api.herokuapp.com/api/schools')
-      .then(res => res.json())
-      .then((data) => {
-          this.setState({ schools: data })
-      })
-      .catch(console.log)
-
-}
 }
 export default App;
