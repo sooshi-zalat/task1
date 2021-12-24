3c8a910230f70bf06e554c50ae96ffa4b37e019d
EsraaZalat <esraasherifzalat@gmail.com>
2021-12-24 14:57:46 +0200

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component} from 'react';
-import Form from './components/Form';
 import axios from 'axios';
 import './App.css';
 import Signin from './components/Signin';
@@ -10,8 +9,7 @@ class App extends Component  {
   return (
     <div className="App">
       <Cascading/>
-     {/* <Form schools={this.state.schools} classes={this.state.classes}/> */}
-     <Signin />
+      <Signin />
     </div>
   );
   }
@@ -31,48 +29,6 @@ componentDidMount() {
       })
       .catch(console.log)
 
-      // fetch('https://react-task-api.herokuapp.com/api/classes')
-      // .then(res => res.json())
-      // .then((data) => {
-      //     this.setState({ classes: data })
-      // })
-      // .catch(console.log)
-
-
-var data = JSON.stringify({
-        "schoolId": "61b605920559dd4a4d5bb0e5"
-      });
-    //   const requestOptions = {
-    //     method: 'get',
-    //     headers: { 'Content-Type': 'application/json' },
-    //     postMessage: JSON.stringify({ data })
-    // };
-    // fetch('https://react-task-api.herokuapp.com/api/classes', requestOptions)
-    //     .then(response => response.json())
-    //     .then(response => console.log(response[0]._id,data));
-
-
-      
-      var config = {
-        method: 'get',
-        url: 'https://react-task-api.herokuapp.com/api/classes/',
-        headers: { 
-          'Content-Type': 'application/json'
-        }
-      };
-      
-      axios({
-                    url:'https://react-task-api.herokuapp.com/api/classes/61b605920559dd4a4d5bb0e5',
-                    headers: { 'Content-Type': 'application/json' }
-      })
-      .then(function (response) {
-        console.log(JSON.stringify(response.data));
-      }).then((data) => {
-             this.setState({ classes: data })
-         })
-      .catch(function (error) {
-        console.log(error);
-      });
 }
 }
 export default App;
--- src/components/Cascading.js
+++ src/components/Cascading.js
@@ -1,160 +1,176 @@
-import React, {Component } from 'react'
-import axios from 'axios';
-import '../App.css';
-import "../style.css"
-import color from "../img/color.jpg"
+import React, { Component } from "react";
+import axios from "axios";
+import "../App.css";
+import "../style.css";
+import color from "../img/color.jpg";
 export class Cascading extends Component {
-constructor(props) {
-super(props)
-this.state = {
-ClassId: '',
-SchoolId: '',
-Schools: [],
-Classes: []
-}
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      ClassId: "",
+      SchoolId: "",
+      Schools: [],
+      Classes: [],
+    };
+  }
 
-componentDidMount() {
+  componentDidMount() {
     var config = {
-        method: 'get',
-        url: 'https://react-task-api.herokuapp.com/api/schools',
-        headers: { }
-      };
-      
-      axios(config)
-      .then(response => {
+      method: "get",
+      url: "https://react-task-api.herokuapp.com/api/schools",
+      headers: {},
+    };
+
+    axios(config)
+      .then((response) => {
         this.setState({
-            Schools: response.data
+          Schools: response.data,
         });
         console.log(JSON.stringify(response.data));
-        }).catch(function (error) {
+      })
+      .catch(function (error) {
         console.log(error);
       });
-    }
+  }
 
-ChangeteState = (e) => {
-    console.log(e.target.value)
-this.setState({
-    SchoolId: e.target.value
-});
-this.state.SchoolId = e.target.value
-if(this.state.SchoolId){
-    let data = JSON.stringify({
-        "schoolId": "61c38c844b3925f593e9505c"
+  ChangeteState = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      SchoolId: e.target.value,
+    });
+    this.state.SchoolId = e.target.value;
+    if (this.state.SchoolId) {
+      let data = JSON.stringify({
+        schoolId: "61c38c844b3925f593e9505c",
       });
       var config = {
-        method: 'get',
-        url: 'https://react-task-api.herokuapp.com/api/classes/'+this.state.SchoolId,
-        headers: { 
-          'Content-Type': 'application/json'
+        method: "get",
+        url:
+          "https://react-task-api.herokuapp.com/api/classes/" +
+          this.state.SchoolId,
+        headers: {
+          "Content-Type": "application/json",
         },
-        data : data
+        data: data,
       };
       axios(config)
-      .then(response => {
-        this.setState({
+        .then((response) => {
+          this.setState({
             Classes: response.data,
+          });
+          console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+          console.log(error);
         });
-        console.log(JSON.stringify(response.data));
-        }).catch(function (error) {
-        console.log(error);
-      });
     }
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <div className="row" className="hdr">
+              <button className="col-sm-12 btn btn-info" disabled>
+                {" "}
+                Spiritude Task
+              </button>
+            </div>
+
+            <div className="tab-content">
+              <div
+                className="tab-pane fade show active"
+                id="pills-login"
+                role="tabpanel"
+                aria-labelledby="tab-login"
+              >
+                <form>
+                  <div className="row mb-4">
+                    <div className="col">
+                      <div className="form-outline">
+                        <label className="form-label">First name</label>
+                        <input
+                          type="text"
+                          id="fname"
+                          className="form-control"
+                        />
+                      </div>
+                    </div>
+                    <div className="col">
+                      <div className="form-outline">
+                        <label className="form-label">Last name</label>
+                        <input
+                          type="text"
+                          id="lname"
+                          className="form-control"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="form-outline mb-4">
+                    <label className="form-label">Email </label>
+                    <input
+                      type="email"
+                      id="loginName"
+                      className="form-control"
+                    />
+                  </div>
+                  <label className="form-label">Schools</label>
+                  <select
+                    className="form-control"
+                    name="school"
+                    value={this.state.SchoolId}
+                    onChange={this.ChangeteState}
+                  >
+                    <option>School Name</option>
+
+                    {this.state.Schools.map((e, key) => {
+                      return (
+                        <option key={key} value={e._id}>
+                          {e.name}
+                        </option>
+                      );
+                    })}
+                  </select>{" "}
+                  <br></br>
+                  <label className="form-label">Classes</label>
+                  <select className="form-control slct" name="class">
+                    <option>Class Name</option>
+                    {this.state.Classes.map((e, key) => {
+                      return (
+                        <option key={key} value={e.id}>
+                          {e.name}
+                        </option>
+                      );
+                    })}
+                  </select>{" "}
+                  <br></br>
+                  <div className="form-outline mb-4">
+                    <label className="form-label">Password</label>
+                    <input
+                      type="password"
+                      id="loginPassword"
+                      className="form-control"
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    className="btn btn-primary btn-block mb-3"
+                  >
+                    Sign up
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-sm-6">
+            <img src={color} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-
-render() {  
-return (  
-
-
-
-
-<div className="container" >
-
-    <div className="row">
-    <div className="col-sm-6">
-    <div className="row" className="hdr">  
-      
-    <button className="col-sm-12 btn btn-info"  disabled> Spiritude Task</button> 
-    
-    </div>  
-
-  <div className="tab-content" >
-   <div
-    className="tab-pane fade show active"
-    id="pills-login"
-    role="tabpanel"
-    aria-labelledby="tab-login"
-   >
-    <form>
-    
-    <div className="row mb-4">
-    <div className="col">
-      <div className="form-outline">
-      <label className="form-label">First name</label>
-        <input type="text" id="fname" className="form-control" />
-   
-      </div>
-    </div>
-    <div className="col">
-      <div className="form-outline">
-      <label className="form-label">Last name</label>
-        <input type="text" id="lname" className="form-control" />
-      
-      </div>
-    </div>
-  </div>
-
-      <div className="form-outline mb-4">
-      <label className="form-label">Email </label>
-        <input type="email" id="loginName" className="form-control" />
-       
-      </div>
-      <label className="form-label">Schools</label>
-<select className="form-control" name="school" value={this.state.SchoolId} onChange={this.ChangeteState}  >  
-<option>School Name</option>
-
-{this.state.Schools.map((e, key) => {  
-return <option key={key} value={e._id}>{e.name}</option>;  
-})}  
-</select>  <br></br>
-<label className="form-label">Classes</label>
-<select className="form-control slct" name="class"  >  
-<option>Class Name</option>
-{this.state.Classes.map((e, key) => { 
-return <option key={key} value={e.id}>{e.name}</option>;  
-})}  
-</select> <br></br>
-
-
-<div className="form-outline mb-4">
-<label className="form-label">Password</label>
-<input type="password" id="loginPassword" className="form-control" />
-
-</div>
-
-<button type="submit" className="btn btn-primary btn-block mb-3">Sign up</button>
-
-
-
-</form>
-</div>
-
-</div>
-</div>
-
-
-<div className="col-sm-6" > 
-
-<img src={color}/>
-</div>
-
-</div>
-
-</div>
-
-
-)  
-}  
-}  
-export default Cascading  
+export default Cascading;
